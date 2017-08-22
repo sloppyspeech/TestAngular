335391d50f22415cf49d67582d1fbb99e036bb4f
SUNGARDAS\ashutosh.mankar <ashutosh.mankar@sungardas.com>
2017-08-22 11:58:37 +0530

    Added a dot

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 var refresh = function() {
   $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
+    console.log("I got the data I requested.");
     $scope.contactlist = response;
     $scope.contact = "";
   });
